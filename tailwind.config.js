a4c29a1f096578a36ec369866f8d559b2eec2670
chetan <chetan.sq05@gmail.com>
2024-07-24 17:40:14 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // darkMode: 'class',
+  //  darkMode: 'media',
   content: ["./main/**/*.{html,js}"],
   theme: {
     extend: {},

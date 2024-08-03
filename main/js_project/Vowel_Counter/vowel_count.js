513a02c8bf85173a51531e3e313c04c60a9f8783
darshan <skillqodedarshan@gmail.com>
2024-08-03 11:20:55 +0530

    calc

--- main/js_project/Vowel_Counter/vowel_count.js
+++ main/js_project/Vowel_Counter/vowel_count.js
@@ -7,33 +7,23 @@ let secondout = '';
 
 function Count() {
 
-
     output.innerText = intext.value;
     secondout = intext.value;
     console.log(secondout);
-    // let a = secondout.length;
-    // console.log(a);
+    
     let count = 0;
-    // let count1 = 0;
+    
     for (let i = 0; i < secondout.length; i++) {
 
         if (secondout[i] == "e" || secondout[i] == "a" || secondout[i] == "i" || secondout[i] == "o" || secondout[i] == "u" || secondout[i] == "A" || secondout[i] == "E" || secondout[i] == "O" || secondout[i] == "U" || secondout[i] == "I") {
 
             count++;
-
         }
-
-
-        // if (secondout[i] == " ") {
-        //     count1++;
-        // }
     }
 
     let count1 = secondout.split(" ");
     count1 = count1.length;
     output.innerText = count;
-    // console.log(count);
-
     word.innerText = count1;
 
 }

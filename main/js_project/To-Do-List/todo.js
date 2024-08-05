071adf2dbda1a76c9dad1218a62543686395ab19
darshan <skillqodedarshan@gmail.com>
2024-08-05 11:57:37 +0530

    to do list

--- main/js_project/To-Do-List/todo.js
+++ main/js_project/To-Do-List/todo.js
@@ -1,18 +1,23 @@
-let intext = document.getElementById('text');
-let ptag = document.getElementById('p');
-
 function create_() {
-    ptag.innerText = intext.value;
+    if (document.getElementById('text').value !== "") {
 
-    var li = document.createElement("li");
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("close");
-
-    console.log(txt);
-    span.className = "close";
-
-    span.appendChild(txt);
-    li.appendChild(span);
+        var div = document.createElement("div");
+        div.classList = `flex justify-between items-center`;
+        div.innerHTML = `<p class="paragraph">${document.getElementById('text').value}</p>
+        <i class="fa-solid fa-xmark deleteButton"></i>`;
 
 
+        document.getElementById("wrapper").appendChild(div);
+
+        document.getElementById('text').value = "";
+    }
+    else {
+        alert("ENTER THE VALID INPUT");
+    }
 }
+
+document.getElementById("wrapper").addEventListener("click", (ansh) => {
+    if (ansh.target.classList.contains("deleteButton")) {
+        ansh.target.parentElement.remove();
+    }
+})

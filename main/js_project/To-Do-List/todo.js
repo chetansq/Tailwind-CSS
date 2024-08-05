977a0d603ba5d212aba6f2f3863331b3e851c89f
chetan <chetan.sq05@gmail.com>
2024-08-05 18:19:38 +0530

    to do list

--- main/js_project/To-Do-List/todo.js
+++ main/js_project/To-Do-List/todo.js
@@ -1,23 +1,51 @@
+
+let add = '';
+let id_ = 1;
+
 function create_() {
-    if (document.getElementById('text').value !== "") {
-
-        var div = document.createElement("div");
-        div.classList = `flex justify-between items-center`;
-        div.innerHTML = `<p class="paragraph">${document.getElementById('text').value}</p>
-        <i class="fa-solid fa-xmark deleteButton"></i>`;
+    if (add == '') {
+        if (document.getElementById('text').value != "") {
 
 
-        document.getElementById("wrapper").appendChild(div);
+            var div = document.createElement("div");
+            div.classList = `flex justify-between items-center`;
+            div.innerHTML = `<p class="paragraph" id="p">${document.getElementById('text').value}</p><i class="fa-solid fa-pen-to-square editButton"></i>
+            <i class="fa-solid fa-xmark deleteButton"></i>`;
 
-        document.getElementById('text').value = "";
+            document.getElementById("wrapper").appendChild(div);
+
+            document.getElementById('text').value = "";
+        }
+        else {
+            alert("ENTER THE VALID INPUT");
+        }
     }
     else {
-        alert("ENTER THE VALID INPUT");
+        console.log(text.value);
+
+        document.getElementById("p").innerText = text.value;
+        // document.getElementById("1").id += 1;
+        document.getElementById('text').value = "";
+        id_++;
+
     }
 }
 
-document.getElementById("wrapper").addEventListener("click", (ansh) => {
-    if (ansh.target.classList.contains("deleteButton")) {
-        ansh.target.parentElement.remove();
+document.getElementById("wrapper").addEventListener("click", (Create_) => {
+    if (Create_.target.classList.contains("deleteButton")) {
+        Create_.target.parentElement.remove();
     }
 })
+
+
+document.getElementById("wrapper").addEventListener("click", (Create_) => {
+    if (Create_.target.classList.contains("editButton")) {
+        text.value = Create_.target.parentElement.innerText;
+        add = text.value;
+    }
+})
+
+
+
+
+

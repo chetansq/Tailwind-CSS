c2f6adae220a8ed5a9c37698d8e5b8760a1a9d4a
chetan <chetan.sq05@gmail.com>
2024-08-07 16:57:00 +0530

    changes

--- main/js_project/To-Do-List/todo.js
+++ main/js_project/To-Do-List/todo.js
@@ -1,16 +1,12 @@
 
-let id_ = 1;
-let d = `p${id_}`;
-console.log(d);
-
 function create_() {
 
-    if (document.getElementById('text').value != "") {
+    if (document.getElementById('text').value != " " && document.getElementById('text').value != "") {
 
 
         var div = document.createElement("div");
-        div.classList = `flex justify-between items-center`;
-        div.innerHTML = `<p contenteditable="false" class="paragraph" id="p${id_}">${document.getElementById('text').value}</p><i class="fa-solid fa-pen-to-square editButton"></i>
+        div.classList = `flex justify-between items-center div`;
+        div.innerHTML = `<p contenteditable="false" class="paragraph" id="p">${document.getElementById('text').value}</p><i class="fa-solid fa-pen-to-square editButton"></i>
             <i class="fa-solid fa-xmark deleteButton"></i>`;
 
         document.getElementById("wrapper").appendChild(div);
@@ -28,15 +24,17 @@ document.getElementById("wrapper").addEventListener("click", (remove_) => {
     }
 })
 
-
-document.getElementById("wrapper").addEventListener("click", (addnew_) => {
-    if (addnew_.target.classList.contains("editButton")) {
-        if (addnew_.target.parentElement.firstChild.getAttribute("contenteditable") == "true") {
-            addnew_.target.parentElement.firstChild.setAttribute("contenteditable", "false")
+document.getElementById("wrapper").addEventListener("click", (add) => {
+    if (add.target.classList.contains("editButton")) {
+        if (add.target.parentElement.firstChild.getAttribute("contenteditable") == "true") {
+            add.target.parentElement.firstChild.setAttribute("contenteditable", "false");
+            add.target.parentElement.firstChild.style.backgroundColor = "rgb(145, 159, 173)";
+            add.target.parentElement.firstChild.style.color = "white";
         }
         else {
-            addnew_.target.parentElement.firstChild.setAttribute("contenteditable", "true")
-
+            add.target.parentElement.firstChild.setAttribute("contenteditable", "true");
+            add.target.parentElement.firstChild.style.backgroundColor = "lightgray";
+            add.target.parentElement.firstChild.style.color = "black";
         }
     }
 })

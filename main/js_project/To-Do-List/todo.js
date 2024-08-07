4ed4a106a332b38cac27804c2c027bf4f869be0d
darshan <skillqodedarshan@gmail.com>
2024-08-07 12:11:14 +0530

    complete todo list

--- main/js_project/To-Do-List/todo.js
+++ main/js_project/To-Do-List/todo.js
@@ -1,36 +1,24 @@
 
-let add = '';
 let id_ = 1;
 let d = `p${id_}`;
 console.log(d);
 
 function create_() {
-    if (add == '') {
-        if (document.getElementById('text').value != "") {
+
+    if (document.getElementById('text').value != "") {
 
 
-            var div = document.createElement("div");
-            div.classList = `flex justify-between items-center`;
-            div.innerHTML = `<p class="paragraph" id="p${id_}">${document.getElementById('text').value}</p><i class="fa-solid fa-pen-to-square editButton"></i>
+        var div = document.createElement("div");
+        div.classList = `flex justify-between items-center`;
+        div.innerHTML = `<p contenteditable="false" class="paragraph" id="p${id_}">${document.getElementById('text').value}</p><i class="fa-solid fa-pen-to-square editButton"></i>
             <i class="fa-solid fa-xmark deleteButton"></i>`;
 
-            document.getElementById("wrapper").appendChild(div);
+        document.getElementById("wrapper").appendChild(div);
 
-            document.getElementById('text').value = "";
-        }
-        else {
-            alert("ENTER THE VALID INPUT");
-        }
+        document.getElementById('text').value = "";
     }
     else {
-        console.log("value 1", text.value);
-
-        document.getElementById(`p${id_}`).innerText = text.value;
-        console.log("value 2", text.value);
-        // document.getElementById("1").id += 1;
-        document.getElementById('text').value = "";
-        id_++;
-
+        alert("ENTER THE VALID INPUT");
     }
 }
 
@@ -43,8 +31,13 @@ document.getElementById("wrapper").addEventListener("click", (remove_) => {
 
 document.getElementById("wrapper").addEventListener("click", (addnew_) => {
     if (addnew_.target.classList.contains("editButton")) {
-        text.value = addnew_.target.parentElement.innerText;
-        add = text.value;
+        if (addnew_.target.parentElement.firstChild.getAttribute("contenteditable") == "true") {
+            addnew_.target.parentElement.firstChild.setAttribute("contenteditable", "false")
+        }
+        else {
+            addnew_.target.parentElement.firstChild.setAttribute("contenteditable", "true")
+
+        }
     }
 })
 
